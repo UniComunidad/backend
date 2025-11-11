b4bed11696fc5bae4e47e403ccc0bcbb11d5377e
LucaLzt <luca.lazarte05@gmail.com>
2025-11-11 18:02:55 -0300

    Spring Boot Base

---
+++ unicomunidad/src/main/java/com/unla/unicomunidad/UnicomunidadApplication.java
@@ -0,0 +1,13 @@
+package com.unla.unicomunidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicomunidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicomunidadApplication.class, args);
+	}
+
+}
---
+++ unicomunidad/src/test/java/com/unla/unicomunidad/UnicomunidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.unicomunidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicomunidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
